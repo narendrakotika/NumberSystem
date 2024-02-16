52c5fe40191a66f5cdf42e3fc892a21bf4304bee
Narendra <narendrakotika3576@gmail.com>
2024-02-16 09:31:07 +0530

    Add project files.

---
+++ NumbersApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using NumbersApp;
+
+Console.WriteLine("Welcome to Number system");
+Console.WriteLine("even numbers ");
+Class1.EvenNum();
+Console.WriteLine("Odd numbers");
+Class1.OddNum();
